b5b023dc24495845dff721f34d6b7b29abd69f38
ArthurEly <arthur.ely78@gmail.com>
2022-03-30 19:24:05 -0300

    alguns testes ai

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*******************************************************************************************
 *
-*   raylib [core] example - Picking in 3d mode
+*   raylib [models] example - Detect basic 3d collisions (box vs sphere vs box)
 *
 *   This example has been created using raylib 1.3 (www.raylib.com)
 *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
@@ -15,51 +15,70 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1000;
+    const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "vamoooussss");
+    InitWindow(screenWidth, screenHeight, "raylib [models] example - box collisions");
 
     // Define the camera to look into our 3d world
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;                   // Camera mode type
+    Camera camera = { { 0.0f, 40.0f, 35.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
 
-    Vector3 cubePosition = { 0.0f, 1.0f, 0.0f };
-    Vector3 cubeSize = { 2.0f, 2.0f, 2.0f };
+    Vector3 playerPosition = { 0.0f, 0.0f, 0.0f };
+    Vector3 playerSize = { 2.0f, 2.0f, 2.0f};
+    Color playerColor = GREEN;
 
-    Ray ray = { 0 };                    // Picking line ray
+    Vector3 enemyBoxPos = { 20, 1.0, -20};
+    Vector3 enemyBoxSize = { 1.0f, 3.0f, 1.0f };
 
-    RayCollision collision = { 0 };
+    Vector3 enemySpherePos = { 4.0f, 0.0f, 0.0f };
+    float enemySphereSize = 1.5f;
 
-    SetCameraMode(camera, CAMERA_FREE); // Set a free camera mode
+    bool collision = false;
 
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
+    while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        UpdateCamera(&camera);          // Update camera
 
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-        {
-            if (!collision.hit)
-            {
-                ray = GetMouseRay(GetMousePosition(), camera);
+        // Move player
+        if (IsKeyDown(KEY_RIGHT)) playerPosition.x += 0.25;
+        else if (IsKeyDown(KEY_LEFT)) playerPosition.x -= 0.25;
+        else if (IsKeyDown(KEY_DOWN)) playerPosition.z += 0.25;
+        else if (IsKeyDown(KEY_UP)) playerPosition.z -= 0.25;
 
-                // Check collision between ray and box
-                collision = GetRayCollisionBox(ray,
-                            (BoundingBox){(Vector3){ cubePosition.x - cubeSize.x/2, cubePosition.y - cubeSize.y/2, cubePosition.z - cubeSize.z/2 },
-                                          (Vector3){ cubePosition.x + cubeSize.x/2, cubePosition.y + cubeSize.y/2, cubePosition.z + cubeSize.z/2 }});
-            }
-            else collision.hit = false;
-        }
+        collision = false;
+
+        // Check collisions player vs enemy-box
+        if (CheckCollisionBoxes(
+            (BoundingBox){(Vector3){ playerPosition.x - playerSize.x/2,
+                                     playerPosition.y - playerSize.y/2,
+                                     playerPosition.z - playerSize.z/2 },
+                          (Vector3){ playerPosition.x + playerSize.x/2,
+                                     playerPosition.y + playerSize.y/2,
+                                     playerPosition.z + playerSize.z/2 }},
+            (BoundingBox){(Vector3){ enemyBoxPos.x - enemyBoxSize.x/2,
+                                     enemyBoxPos.y - enemyBoxSize.y/2,
+                                     enemyBoxPos.z - enemyBoxSize.z/2 },
+                          (Vector3){ enemyBoxPos.x + enemyBoxSize.x/2,
+                                     enemyBoxPos.y + enemyBoxSize.y/2,
+                                     enemyBoxPos.z + enemyBoxSize.z/2 }})) collision = true;
+
+        // Check collisions player vs enemy-sphere
+        if (CheckCollisionBoxSphere(
+            (BoundingBox){(Vector3){ playerPosition.x - playerSize.x/2,
+                                     playerPosition.y - playerSize.y/2,
+                                     playerPosition.z - playerSize.z/2 },
+                          (Vector3){ playerPosition.x + playerSize.x/2,
+                                     playerPosition.y + playerSize.y/2,
+                                     playerPosition.z + playerSize.z/2 }},
+            enemySpherePos, enemySphereSize)) collision = true;
+
+        if (collision) playerColor = RED;
+        else playerColor = GREEN;
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -70,29 +89,24 @@ int main(void)
 
             BeginMode3D(camera);
 
-                if (collision.hit)
-                {
-                    DrawCube(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, RED);
-                    DrawCubeWires(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, MAROON);
+                // Draw enemy-box
+                DrawCube(enemyBoxPos, enemyBoxSize.x, enemyBoxSize.y, enemyBoxSize.z, GRAY);
+                DrawCubeWires(enemyBoxPos, enemyBoxSize.x, enemyBoxSize.y, enemyBoxSize.z, DARKGRAY);
 
-                    DrawCubeWires(cubePosition, cubeSize.x + 0.2f, cubeSize.y + 0.2f, cubeSize.z + 0.2f, GREEN);
-                }
-                else
-                {
-                    DrawCube(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, GRAY);
-                    DrawCubeWires(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, DARKGRAY);
-                }
+                // Draw enemy-sphere
+                DrawSphere(enemySpherePos, enemySphereSize, GRAY);
+                DrawSphereWires(enemySpherePos, enemySphereSize, 16, 16, DARKGRAY);
 
-                DrawRay(ray, MAROON);
-                DrawGrid(10, 1.0f);
+                // Draw player
+                DrawCubeV(playerPosition, playerSize, playerColor);
+
+                DrawGrid(40, 1.0f);        // Draw a grid
 
             EndMode3D();
 
-            DrawText("clica ai na caixinha!", 240, 10, 20, DARKGRAY);
+            DrawText("Move player with cursors to collide", 220, 40, 20, GRAY);
 
-            if (collision.hit) DrawText("parabens", (screenWidth - MeasureText("parabens", 30)) / 2, (int)(screenHeight * 0.1f), 30, GREEN);
-
-            DrawFPS(10, 10);
+            DrawFPS(100, 10);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
